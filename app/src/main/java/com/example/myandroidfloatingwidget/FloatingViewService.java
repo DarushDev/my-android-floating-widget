a28edb13b2521105fa6f41f0bcad3318776fb9a7
DarushDev <darushfaqiri@gmail.com>
2017-07-18 15:10:49 +0800

    Handle touch events for floating widget

--- app/src/main/java/com/example/myandroidfloatingwidget/FloatingViewService.java
+++ app/src/main/java/com/example/myandroidfloatingwidget/FloatingViewService.java
@@ -6,6 +6,7 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -111,6 +112,38 @@ public class FloatingViewService extends Service {
 
                 //close the service and remove view from the view hierarchy
                 stopSelf();
+            }
+        });
+
+        //Drag and move floating view using user's touch action.
+        mFloatingView.findViewById(R.id.root_container).setOnTouchListener(new View.OnTouchListener() {
+            private int initialX;
+            private int initialY;
+            private float initialTouchX;
+            private float initialTouchY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //remember the initial position.
+                        initialX = params.x;
+                        initialY = params.y;
+
+                        //get the touch location
+                        initialTouchX = event.getRawX();
+                        initialTouchY = event.getRawY();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        //Calculate the X and Y coordinates of the view.
+                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
+                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
+
+                        //Update the layout with new X & Y coordinate
+                        mWindowManager.updateViewLayout(mFloatingView, params);
+                        return true;
+                }
+                return false;
             }
         });
     }
